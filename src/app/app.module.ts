47490d57c97589669aa57b25e9adc75be4707f23
Sarahinwn <riej98@gmail.com>
2021-02-17 17:12:53 -0600

    page-not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'crisis-list', component: CrisisListComponent},
       {path: 'heroes-list', component: HeroesListComponent},
-      {path: '', redirectTo: '/heroes-list', pathMatch: 'full'}
+      {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
     ]),
   ],
   providers: [],
