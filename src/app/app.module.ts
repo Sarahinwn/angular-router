8c7d157c35e22aac1077e3029d30ee6fa288d46d
Sarahinwn <riej98@gmail.com>
2021-02-17 15:16:51 -0600

    ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'crisis-list', component: CrisisListComponent},
+      {path: 'heroes-list', component: HeroesListComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
